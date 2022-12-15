690ee54ab54599019c118782d670ce2cc130de42
scsibranpeti <scsibran.peter.balint@gmail.com>
2022-12-15 09:19:57 +0100

    Add project files.

---
+++ csharpzh/NGC.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpzh
+{
+    internal class NGC
+    {
+        //1=átlagos, 2=optimista, 3=pesszimista 
+        public int type { get; set; }
+        public float happines { get; set; }
+
+        public NGC()
+        {
+            Random random = new Random();
+            float rnd = (float)(random.NextDouble()*10);
+            if (rnd <= 6.7)
+            {
+                this.type = 1;
+            }
+            else if (rnd > 6.7 && rnd <= 8.7)
+            {
+                this.type = 2;
+            }
+            else
+            {
+                this.type = 3;
+            }
+
+            switch (type)
+            {
+                case 1:
+                    this.happines = (float)(random.NextDouble()*100);
+                    break;
+                case 2:
+                    this.happines = (float)(60 + random.NextDouble() * 40);
+                    break;
+                case 3:
+                    this.happines = (float)(random.NextDouble() * 20);
+                    break;
+            }
+        }
+    }
+}
---
+++ csharpzh/Program.cs
@@ -0,0 +1,29 @@
+﻿using csharpzh;
+
+List<NGC> list = new List<NGC>();
+int listSize = 10000000;
+
+for(int i = 0; i<listSize; i++)
+{
+    list.Add(new NGC());
+}
+
+float sum = 0;
+int opt = 0;
+int atl = 0;
+int pessz = 0;
+foreach(NGC ngiszi in list)
+{
+    sum += ngiszi.happines;
+    switch (ngiszi.type)
+    {
+        case 1: atl++;
+            break;
+        case 2: opt++;
+            break;
+        case 3: pessz++;
+            break;
+
+    }
+}
+Console.WriteLine("összboldogság: " + sum + " optimista: " + ((float)opt/listSize)*100 + "% , pesszimista: " + ((float)pessz/listSize)*100 + "% , átlagos: " + ((float)atl/listSize)*100 + "%");
